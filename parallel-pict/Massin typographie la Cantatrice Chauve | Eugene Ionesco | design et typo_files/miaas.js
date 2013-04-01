808bb4507037719557ea89e6b0041714a35d3508
Ludi <hello@ludi.be>
2013-04-01 14:02:10 +0200

    session 2

--- parallel-pict/Massin typographie la Cantatrice Chauve | Eugene Ionesco | design et typo_files/miaas.js
+++
@@ -1,9 +0,0 @@
-$jq = jQuery.noConflict();
-$jq(document).ready(function($jq){     
-$jq('#commentform').submit(function(){
-      $jq(this).append($jq('<input />').attr('name','token').attr('id','token').attr('type','hidden'));      
-      $jq('#token').val(miaas);      
-      });         
-});
-
-
---
+++ references/Massin typographie la Cantatrice Chauve | Eugene Ionesco | design et typo_files/fpc.js
@@ -0,0 +1,5 @@
+// First Party Cookies
+// TS: 0 255 onodc4-ac4
+
+window.ITTs[0].FPCV = 'ZatgDm2G|YUHUrkTMaa|fses10001467053656=|YUHUrkTMaa|ZatgDm2G|fvis10001467053656=|8M0Mo0Ho7s|8M0Mo0Ho7s|8M0Mo0Ho7s|8|8M0Mo0Ho7s|8M0Mo0Ho7s';
+window.ITTs[0].setFPCookies();
