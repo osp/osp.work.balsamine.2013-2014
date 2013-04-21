422f5d8fda599f733b74a1786d426022611725d9
Ludi <hello@ludi.be>
2013-04-21 02:25:32 +0200

    down pad but local colors

---
+++ photos/layers/save-layers-as-images.py
@@ -0,0 +1,11 @@
+#à utiliser depuis la console python dans Gimp
+
+def save_all_layers(image, directory, name_pattern):
+    for layer in image.layers:
+        filename = directory + (name_pattern % layer.name)
+        raw_filename = name_pattern % layer.name
+        pdb.gimp_file_save(image, layer, filename, raw_filename)
+
+gimp.image_list()
+img = gimp.image_list()[0]
+save_all_layers(img, "/path/to/save/directory/", "image_%s.png")
