8876b8938b6f57ab8193b88c8314700a2dc24112
Stéphanie Vilayphiou <stephanie@stdin.fr>
2013-04-25 23:55:43 +0200

    un script python pour mixer 2 UFOs en un.
    Usage:
    
        `python 2fontsin1.py fonte1.ufo fonte2.ufo fonte-out.ufo`
    
    Résultat: une Ume Mincho extrudée avec surface noire.

---
+++ fonts/2fontsin1.py
@@ -0,0 +1,26 @@
+#! /usr/bin/python
+
+"""
+Usage:
+    python 2fontsin1.py fonte1.ufo fonte2.ufo fonte-out.ufo
+"""
+
+
+import fontforge
+import psMat
+import math
+import sys
+
+matrix = psMat.rotate(math.radians(45))
+
+font1 = fontforge.open(sys.argv[1])
+font2 = fontforge.open(sys.argv[2])
+
+for glyph in font1.glyphs():
+    #glyph.wireframe(100, 300, -45)
+    font1.selection.select(glyph.glyphname)
+    font1.copy()
+    font2.selection.select(glyph.glyphname)
+    font2.pasteInto()
+
+font2.generate(sys.argv[3])
